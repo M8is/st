e51f5a4021a4d43b87a95d381a1cb47ebcb4e4c9
Mattis Kaemmerer <mattis.kaemmerer@gmx.de>
2020-02-21 22:17:58 +0100

    Use base16 nord colorscheme

--- config.h
+++ config.h
@@ -84,38 +84,31 @@ unsigned int tabspaces = 8;
 
 /* Terminal colors (16 first used in escape sequence) */
 static const char *colorname[] = {
-	/* gruvbox colorscheme (https://github.com/morhetz/gruvbox) */
-	"#282828", /* hard contrast: #1d2021 / soft contrast: #32302f */
-	"#cc241d",
-	"#98971a",
-	"#d79921",
-	"#458588",
-	"#b16286",
-	"#689d6a",
-	"#a89984",
-	"#928374",
-	"#fb4934",
-	"#b8bb26",
-	"#fabd2f",
-	"#83a598",
-	"#d3869b",
-	"#8ec07c",
-	"#ebdbb2",
-	[255] = 0,
-	/* more colors can be added after 255 to use with DefaultXX */
-	"#282828",   /* 256 -> bg */
-	"#ebdbb2",   /* 257 -> fg */
-	"#add8e6", /* 258 -> cursor */
+  "#2e3440", /* base00 */
+  "#88c0d0", /* base08 */
+  "#bf616a", /* base0B */
+  "#5e81ac", /* base0A */
+  "#ebcb8b", /* base0D */
+  "#a3be8c", /* base0E */
+  "#d08770", /* base0C */
+  "#e5e9f0", /* base05 */
+  "#4c566a", /* base03 */
+  "#81a1c1", /* base09 */
+  "#3b4252", /* base01 */
+  "#434c5e", /* base02 */
+  "#d8dee9", /* base04 */
+  "#eceff4", /* base06 */
+  "#b48ead", /* base0F */
+  "#8fbcbb", /* base07 */
 };
-
 
 /*
  * Default colors (colorname index)
  * foreground, background, cursor, reverse cursor
  */
-unsigned int defaultfg = 257;
-unsigned int defaultbg = 256;
-static unsigned int defaultcs = 258;
+unsigned int defaultfg = 7;
+unsigned int defaultbg = 0;
+static unsigned int defaultcs = 13;
 static unsigned int defaultrcs = 0;
 
 /*
